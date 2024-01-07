7f509157bde83e0e610b8f52d5d5654d5e6093d8
Vittorio Sassu <v.sassu48@gmail.com>
2024-01-07 21:24:01 +0100

    Initial Commit

---
+++ Hangman/HangmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangmanApp.swift
+//  Hangman
+//
+//  Created by Vittorio Sassu on 07/01/24.
+//
+
+import SwiftUI
+
+@main
+struct HangmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
