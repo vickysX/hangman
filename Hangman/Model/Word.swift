7ea51394fba6ef6fee38c85eac958d41c4126fbb
Vittorio Sassu <v.sassu48@gmail.com>
2024-01-21 15:57:30 +0100

    add property definitionSource and populate some data

--- Hangman/Model/Word.swift
+++ Hangman/Model/Word.swift
@@ -7,11 +7,13 @@
 
 import Foundation
 
-
+// Codable protocol combines both Encodable and Decodable protocols
+// Types conforming to Codable are also conforming to Encodable and Decodable
 struct Word: Codable, Hashable, Identifiable {
     var id: Int
     var entry: String
     var definition: String
+    var definitionSource: String
     var level: DifficultyLevel
     
     enum DifficultyLevel: String, Codable {
